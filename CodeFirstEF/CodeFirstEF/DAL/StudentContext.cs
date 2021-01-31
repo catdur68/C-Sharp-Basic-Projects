c374244e4ce81dc2254bada7920906bace080e3f
catdur68 <66445106+catdur68@users.noreply.github.com>
2021-01-31 12:09:15 -0800

    commit

---
+++ CodeFirstEF/CodeFirstEF/DAL/StudentContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstEF.Models
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext(): base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+
+    
+}
--- NewsletterAppMVC/NewsletterAppMVC/dbo.SignUps.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[SignUps] (
-    [Id]           INT           IDENTITY (1, 1) NOT NULL,
-    [FirstName]    VARCHAR (50)  NULL,
-    [LastName]     VARCHAR (50)  NULL,
-    [EmailAddress] VARCHAR (100) NULL, 
-    [SocialSecurityNumber] VARCHAR(15) NULL
-);
-
